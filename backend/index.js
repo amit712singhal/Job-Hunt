4b58ef7a54533d004245d2ba522e8f5a6d74dfaf
amit712singhal <rakshit.singhal712@gmail.com>
2024-09-26 13:08:37 +0530

    Update backend and frontend API endpoints

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use( express.json() );
 app.use( express.urlencoded( { extended: true } ) );
 app.use( cookieParser() );
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://job-hunt-zzgb.onrender.com',
   credentials: true
 }
 
