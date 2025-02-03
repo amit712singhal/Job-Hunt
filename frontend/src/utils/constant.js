3b783ed8bb4f70dda86fa0513288965f83f58d00
amit712singhal <rakshit.singhal712@gmail.com>
2025-02-04 00:04:16 +0530

    Add API endpoint constants for user, job, application, and company

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,4 @@
+export const USER_API_END_POINT = `http://localhost:${ import.meta.env.VITE_API_PORT }/api/v1/user`;
+export const JOB_API_END_POINT = `http://localhost:${ import.meta.env.VITE_API_PORT }/api/v1/job`;
+export const APPLICATION_API_END_POINT = `http://localhost:${ import.meta.env.VITE_API_PORT }/api/v1/application`;
+export const COMPANY_API_END_POINT = `http://localhost:${ import.meta.env.VITE_API_PORT }/api/v1/company`;
