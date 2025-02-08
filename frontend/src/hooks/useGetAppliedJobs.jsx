ca2eeab6d38f79056d5190b89390d102bcf04d31
amit712singhal <rakshit.singhal712@gmail.com>
2025-02-08 11:58:10 +0530

    Add custom hook for fetching applied jobs from API

---
+++ frontend/src/hooks/useGetAppliedJobs.jsx
@@ -0,0 +1,24 @@
+import { setAllAppliedJobs } from "@/redux/jobSlice";
+import { APPLICATION_API_END_POINT } from "@/utils/constant";
+import axios from "axios"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+
+const useGetAppliedJobs = () => {
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        const fetchAppliedJobs = async () => {
+            try {
+                const res = await axios.get(`${APPLICATION_API_END_POINT}/get`, {withCredentials:true});
+                if(res.data.success){
+                    dispatch(setAllAppliedJobs(res.data.application));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchAppliedJobs();
+    },[])
+};
+export default useGetAppliedJobs;
